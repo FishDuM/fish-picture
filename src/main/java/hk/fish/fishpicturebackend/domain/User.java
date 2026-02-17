1133f1aca456dde3b39119810df765ad7468cdbc
会飞的鱼 <3057433102@qq.com>
2026-02-17 18:29:12 +0800

    mybatisX生成代码

--- src/main/java/hk/fish/fishpicturebackend/domain/User.java
+++ src/main/java/hk/fish/fishpicturebackend/domain/User.java
@@ -1,9 +1,7 @@
 package hk.fish.fishpicturebackend.domain;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.util.Date;
 import lombok.Data;
 
@@ -17,7 +15,7 @@ public class User {
     /**
      * id
      */
-    @TableId(type = IdType.AUTO)
+    @TableId(type = IdType.ASSIGN_ID)
     private Long id;
 
     /**
@@ -68,5 +66,6 @@ public class User {
     /**
      * 是否删除
      */
+    @TableLogic
     private Integer isDelete;
 }
