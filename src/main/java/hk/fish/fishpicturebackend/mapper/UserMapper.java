0ddcf8b62c62f667e4b50bb706a060a77331ab92
会飞的鱼 <3057433102@qq.com>
2026-02-17 18:23:47 +0800

    mybatisX生成代码

---
+++ src/main/java/hk/fish/fishpicturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package hk.fish.fishpicturebackend.mapper;
+
+import hk.fish.fishpicturebackend.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 30574
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2026-02-17 18:20:55
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/hk/fish/fishpicturebackend/service/UserService.java
@@ -0,0 +1,13 @@
+package hk.fish.fishpicturebackend.service;
+
+import hk.fish.fishpicturebackend.domain.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 30574
+* @description 针对表【user(用户)】的数据库操作Service
+* @createDate 2026-02-17 18:20:55
+*/
+public interface UserService extends IService<User> {
+
+}
